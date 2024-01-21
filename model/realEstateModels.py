6ca1d7ab798fbff6c275186c8ce13f275d51694d
FIRST_NAME LAST_NAME <nitinsandiego@gmail.com>
2024-01-21 12:48:01 -0800

    Update realEstateModels.py

--- model/realEstateModels.py
+++ model/realEstateModels.py
@@ -33,3 +33,80 @@ class Account(db.Model, UserMixin):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
     
+
+class House(db.Model, UserMixin):
+    __tablename__ = 'houses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(256), index=True)
+    city = db.Column(db.String(64), index=True, nullable=True)
+    state = db.Column(db.String(8), index=True, nullable=True)
+    zip = db.Column(db.Integer, index=True, nullable=True)
+    latitude = db.Column(db.Float, index=True, nullable=True)
+    longitude = db.Column(db.Float, index=True, nullable=True)
+    price = db.Column(db.Integer, index=True, nullable=True)
+    bathrooms = db.Column(db.Float, index=True, nullable=True)
+    bedrooms = db.Column(db.Integer, index=True, nullable=True)
+    livingarea = db.Column(db.Integer, index=True, nullable=True)
+    homeType = db.Column(db.String(64), index=True, nullable=True)
+    priceEstimate = db.Column(db.Integer, index=True, nullable=True)
+    rentEstimate = db.Column(db.Integer, index=True, nullable=True)
+    imgSRC = db.Column(db.String, index=True, nullable=True)
+    favorites = db.relationship('Favorite', backref='House', uselist=True, lazy='dynamic')
+
+    def __init__(self, address, city, state, zip, latitude, longitude, price, bathrooms, bedrooms, livingarea, homeType, priceEstimate, rentEstimate, imgSRC):
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.latitude = latitude
+        self.longitude = longitude
+        self.price = price
+        self.bathrooms = bathrooms
+        self.bedrooms = bedrooms
+        self.livingarea = livingarea
+        self.homeType = homeType
+        self.priceEstimate = priceEstimate
+        self.rentEstimate = rentEstimate
+        self.imgSRC = imgSRC
+    
+    def all_details(self):
+        return {
+            'id': self.id,
+            'address': self.address,
+            'city': self.city,
+            'state': self.state,
+            'zip': self.zip,
+            'latitude': self.latitude,
+            'longitude': self.longitude,
+            'price': self.price,
+            'bathrooms': self.bathrooms,
+            'bedrooms': self.bedrooms,
+            'livingarea': self.livingarea,
+            'homeType': self.homeType,
+            'priceEstimate': self.priceEstimate,
+            'rentEstimate': self.rentEstimate,
+            'imgSRC': self.imgSRC
+        }
+    
+    def few_details(self):
+        return {
+            'id': self.id,
+            'address': self.address,
+            'price': self.price,
+            'livingarea': self.livingarea,
+            'bathrooms': self.bathrooms,
+            'bedrooms': self.bedrooms,
+            'imgSRC': self.imgSRC
+        }
+    
+
+class Favorite(db.Model):
+    __tablename__ = 'favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    account_id = db.Column(db.Integer, db.ForeignKey('accounts.account_id'))
+    house_id = db.Column(db.Integer, db.ForeignKey('houses.id'))
+
+    def __init__(self, account_id, house_id):
+        self.account_id = account_id
+        self.house_id = house_id
