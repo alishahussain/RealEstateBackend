c44595543c974b7937828d11674c9257d9616263
FIRST_NAME LAST_NAME <nitinsandiego@gmail.com>
2024-01-23 10:04:13 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from flask_cors import CORS
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 import os
 
 """
@@ -10,18 +11,25 @@ These object can be used throughout project.
 2.) Isolating these object definitions avoids duplication and circular dependencies
 """
 
+login_manager = LoginManager()
+
 # Setup of key Flask object (app)
 app = Flask(__name__)
 cors = CORS(app, supports_credentials=True)
 
 # Setup SQLAlchemy object and properties for the database (db)
-dbURI = 'sqlite:///volumes/sqlite.db'
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+# Setup SQLAlchemy object and properties for the database (db)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+print(app.config["SQLALCHEMY_DATABASE_URI"])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = dbURI
-SECRET_KEY = os.environ.get('SECRET_KEY') or 'SECRET_KEY'
-app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SECRET_KEY'] = 'SECRET_KEY'
 db = SQLAlchemy()
 Migrate(app, db)
+login_manager.init_app(app)
+
 
 # Images storage
 app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024  # maximum size of uploaded content
