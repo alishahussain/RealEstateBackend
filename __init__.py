f81263195ff5edfa0e7a34b19b9342da54cca045
FIRST_NAME LAST_NAME <nitinsandiego@gmail.com>
2024-02-10 20:02:21 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ These object can be used throughout project.
 
 # Setup of key Flask object (app)
 app = Flask(__name__)
-cors = CORS(app, supports_credentials=True)
+cors = CORS(app, supports_credentials=True, methods=["GET", "POST", "PUT", "DELETE"])
 #CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}})
 
 # Setup SQLAlchemy object and properties for the database (db)
